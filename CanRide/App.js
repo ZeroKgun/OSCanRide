6c6aa9ae4b9f79f6ecac7237d2e3af9df19fa0a7
kirin2211 <kirin2211@naver.com>
2022-05-29 21:38:42 +0900

    Second Commit

--- CanRide/App.js
+++ CanRide/App.js
@@ -1,20 +1,94 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+//import MapView from "react-native-maps";
+import { StyleSheet, View, Dimensions, Alert } from "react-native";
+import styled from "styled-components";
+import { useState, useEffect } from "react";
+import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
+import * as Location from "expo-location";
+import metro from "./metro.json";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
+const desc = [];
+
+const app = ({ navigation }) => {
+  const [initialRegion, setInitialRegion] = useState({
+    latitude: 37.548014,
+    longitude: 127.074658,
+    latitudeDelta: 5,
+    longitudeDelta: 5,
+  });
+  const [mapWidth, setMapWidth] = useState("99%");
+  const [location, setLocation] = useState(0);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [inputText, setInputText] = useState("");
+
+  const ccode = {};
+
+  const updateMapStyle = () => {
+    setMapWidth("100%");
+  };
+
+  // Get current location information
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = "Waiting..";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+    console.log("[LOG] current location : " + text);
+    console.log(inputText);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Wrapper style={{ backgroundColor: "white" }}>
+      <View style={styles.centeredView}>
+        <MapView
+          initialRegion={initialRegion}
+          style={[styles.map, { width: SCREEN_WIDTH }]}
+          provider={PROVIDER_GOOGLE}
+          showsUserLocation={true}
+          showsMyLocationButton={true}
+          onMapReady={() => {
+            updateMapStyle();
+          }}
+        ></MapView>
+      </View>
+    </Wrapper>
   );
-}
+};
 
+export default app;
+
+const Wrapper = styled.View`
+  flex: 1;
+  flex-direction: column;
+  background: rgba(0, 0, 0, 0.8);
+`;
 const styles = StyleSheet.create({
-  container: {
+  map: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: "100%",
+    height: "100%",
+  },
+
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
   },
 });
